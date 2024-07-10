0e1cacb3cada5d7c9b86075a5e9465dee87c6395
isedt <isedt@hotmail.com>
2024-07-10 15:11:02 -0600

    Added dani request

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-
-function countdown() {
+function startCountdown() {
     var count = 3;
     var countdownElement = document.getElementById('countdown');
+    var proposalButton = document.getElementById('proposal-button');
+
+    proposalButton.style.display = 'none';
+
+    countdownElement.style.display = 'inline-block';
 
     var interval = setInterval(function() {
         countdownElement.textContent = count;
         count--;
         if (count < 0) {
             clearInterval(interval);
-            countdownElement.style.display = 'none';
-            var proposalButton = document.getElementById('proposal-button');
-            proposalButton.style.display = 'inline-block';
-        }
-    }, 1000);
+            countdownElement.style.display = 'none'; // Hide countdown after countdown finishes
+            var proposalText = document.getElementById('proposal-text');
+            proposalText.style.display = 'block'; // Show proposal text
+        } 
+    }, 800);
 }
-
-document.getElementById('countdown').addEventListener('click', countdown);
