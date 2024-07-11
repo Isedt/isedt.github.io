6b27e80c6c95bb6fdab5a3c50f5fdfa84f20c560
isedt <isedt@hotmail.com>
2024-07-11 10:58:26 -0600

    fix button2

---
+++ scriptreload.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var apiButton = document.getElementById('apiButton');
+  
+    apiButton.addEventListener('click', function(event) {
+      //event.preventDefault();
+  
+      setTimeout(function() {
+        location.reload(); 
+      }, 4000);
+    });
+  });
+  
